a5c801b71b03744d2c43092ea49244856ef2ae27
Neha panbude <nehapanbude123@gmail.com>
2024-10-09 12:22:33 +0530

    string'js first commit

---
+++ 9string.js
@@ -0,0 +1,13 @@
+let str = 'We are developers';
+let str1 = "we are developers";
+let str2 = `we are developers`;
+console.log(str);
+console.log(str[0]);
+console.log(str[8]);
+count = 0;
+for (let i = 0;i<str.length;i++){
+    console.log(i);
+    console.log(str[i]);
+    count++;
+}
+console.log(count);
